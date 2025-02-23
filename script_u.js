6630b0ca138aa71aab3470d8f6416eadc65a73d7
Papai <151509513+papuai@users.noreply.github.com>
2025-02-23 19:20:20 +0530

    Update script_u.js

--- script_u.js
+++ script_u.js
@@ -1,3 +1,10 @@
 function sayHello() {
     alert("Hello! Thanks for visiting my portfolio!");
 }
+const myElement = document.getElementById('button-div'); // Replace with your element's ID
+
+if(myElement){
+  myElement.addEventListener('contextmenu', function(event) {
+    event.preventDefault(); // Prevent the default context menu
+  });
+}
